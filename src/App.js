c16df131fd65e99fb29c7f57b09b9834b1aba6bd
Rushmitkarki <220046@softwarica.edu.np>
2024-04-26 09:45:54 +0545

    page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
-import './App.css';
 
 function App() {
   return (
-    <div >
-    <h1> welcome to our app     </h1>
-    hello
-    </div>
+    
+    <Router>
+      <Routes>
+        <Route path='/Homepage' element = {<Homepage/>}/>
+        <Route path='/Login' element = {<Login/>}/>
+        <Route path='/Register' element = {<Register/>}/>
+      </Routes>
+    </Router>
   );
 }
 
